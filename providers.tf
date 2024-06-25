199453a9b17ebb66770e08b22aebe3e6639de792
alirionx <dquilitzsch@outlook.de>
2024-06-25 22:49:38 +0200

    provider update

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.31.0"
+    }
+  }
+}
+
 provider "kubernetes" {
   host = var.kube_api_url
   token = var.kube_token
